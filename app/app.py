782f5852131411d6faeade19062104243b985bf8
Andrew J Freyer <andrew.freyer@gmail.com>
2020-04-01 11:54:55 -0600

    setup example configs

--- app/app.py
+++ app/app.py
@@ -1,41 +1,41 @@
 import os, json
 from flask import Flask, render_template, request
 
-#FLASK
+#========== FLASK 
 app = Flask(__name__, template_folder='/app/data')
 config = {}
 
-#LOAD CONFIGURATION FILE
+#========== LOAD CONFIGURATION FILE
 try: 
     #open configuration file
     with open('../app/data/config.json') as json_file:
         config = json.load(json_file)
-        print ("Loaded: " + str(config.length()) + " appliance configuration(s).") 
-
+        print ("Loaded: %s appliance configuration(s).", str(len(config))) 
 except:
-    print ("Error: Cannot load configuration file.")
+    print ("Error: Cannot load configuration file or appliance configuration(s).")
 
-
-#primary route
+#========== DEFINE SIMPLE PRIMARY ROUTE
 @app.route("/q", methods=['GET'])
 def query():
     """ This function serves a webpage with appliance config information"""
 
+    #--- GET VALUES 
     q_type = str(request.args.get('t'))
     name = str(request.args.get('name'))
 
     #only supporting one request time right now
     if (q_type.lower() == "appliance"):
 
-        #get this devices config
+        #--- LOAD DEVICE CONFIG
         requested_data = config[name]
 
-        #render template
+        #--- RENDER TEMPLATE
         return render_template(
             'index.html',
             data = requested_data)
  
-    return "Unknown error"
+    #INTENTIONALLY EMPTY RESPONSE
+    return ('', 204)
 
 #MAIN
 if __name__ == "__main__":
